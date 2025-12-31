d6765394335085aca101bc805a7d90407637ceaa
danimolina-ai <danifocus@users.noreply.github.com>
2025-12-31 11:52:32 +0000

    Add screens barrel export (index.js)

---
+++ src/screens/index.js
@@ -0,0 +1,21 @@
+// Screens barrel export
+// All screens extracted from AppPage.jsx
+
+export { default as BodyScreen } from './BodyScreen';
+export { default as CalendarScreen } from './CalendarScreen';
+export { default as ConsciousnessScreen } from './ConsciousnessScreen';
+export { default as ControlScreen } from './ControlScreen';
+export { default as FinancesScreen } from './FinancesScreen';
+export { default as GoalsScreen } from './GoalsScreen';
+export { default as HabitsScreen } from './HabitsScreen';
+export { default as MealsScreen } from './MealsScreen';
+export { default as OnboardingScreen } from './OnboardingScreen';
+export { default as PersonalScreen } from './PersonalScreen';
+export { default as RelationshipsScreen } from './RelationshipsScreen';
+export { default as SettingsScreen } from './SettingsScreen';
+export { default as SpiritScreen } from './SpiritScreen';
+export { default as StatsScreen } from './StatsScreen';
+export { default as TodayScreen } from './TodayScreen';
+export { default as WeeklyScreen } from './WeeklyScreen';
+export { default as WorkoutScreen } from './WorkoutScreen';
+export { default as WorkScreen } from './WorkScreen';
